c740870b113f0f8447283dc13f1496d80ba7d9f7
Igor Thygypalo <tigory8@gmail.com>
2019-05-07 22:38:16 +0000

    Init

--- js/index.js
+++ js/index.js
@@ -58,3 +58,43 @@ for(const product of products) {
     priceList.appendChild(productPara);
 };
 
+
+const clockToggleButton = document.querySelector('.clockToggle');
+clockToggleButton.addEventListener('click', clockToggle);
+
+function clockToggle (e) {
+    e.preventDefault();
+    clockContainer.hidden = !clockContainer.hidden;
+    clockToggleButton.innerText = clockContainer.hidden ? 'Show clock' : 'Hide clock';
+};
+
+
+
+
+const display = document.querySelector('.display');
+const digits = document.querySelectorAll('.digits button');
+digits.forEach( btn => btn.addEventListener('click', digitPressed) );
+function digitPressed(e) {
+    e.preventDefault();
+    display.value += e.target.innerText;
+}
+const opers = document.querySelectorAll('.opers button');
+opers.forEach( btn => btn.addEventListener('click', operPressed) );
+function operPressed(e) {
+    e.preventDefault();
+    display.value += e.target.innerText;
+}
+
+document.querySelector('.equal').addEventListener('click', equalPressed);
+function equalPressed(e) {
+    e.preventDefault();
+    display.value = eval(display.value);
+    
+}
+
+document.querySelector('.clear').addEventListener('click', clearPressed);
+function clearPressed(e) {
+    e.preventDefault();
+    display.value = '';
+}
+
